8911548ee72544a5f3fe6f8d26e7d72fb2c1cc15
avilard4 <avilard4@jhu.edu>
2015-02-11 12:14:01 -0500

    Removes the copy to portlets lib folder only to portlet-ext

--- src/main/java/edu/jhu/cvrg/filestore/filetree/HadoopFileTree.java
+++ src/main/java/edu/jhu/cvrg/filestore/filetree/HadoopFileTree.java
@@ -28,4 +28,10 @@ public class HadoopFileTree extends FileTree{
 	public FileNode getRoot() {
 		return null;
 	}
+
+	@Override
+	public FileNode getFileNodeByName(String fileName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
